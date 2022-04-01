26d101fb94824ab043e5ee78a18866e1e1688f3c
Kui-Feng Lee <kuifeng@fb.com>
2022-04-01 11:50:24 -0700

    Generate C header at build time.
    Call cbindgen in build.rs to generate blazesym.h at the target
    directory.
    
    Signed-off-by: Kui-Feng Lee <kuifeng@fb.com>

---
+++ build.rs
@@ -0,0 +1,17 @@
+extern crate cbindgen;
+
+use std::env;
+use std::path::Path;
+
+fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let build_type = env::var("PROFILE").unwrap();
+    let target_path = Path::new(&crate_dir).join("target").join(build_type);
+
+    cbindgen::Builder::new()
+	.with_crate(&crate_dir)
+	.with_config(cbindgen::Config::from_root_or_default(&crate_dir))
+	.generate()
+	.expect("Unable to generate bindings")
+	.write_to_file(target_path.join("blazesym.h"));
+}
