df9785abcfb59611121a99c8cc226ea39fcfb5b4
Kui-Feng Lee <kuifeng@fb.com>
2022-05-12 13:37:46 -0700

    Add feature 'cheader' to generate C headers.
    The feature 'cheader' enables to generate C headers at build time
    instead of using environment variables.
    
    Signed-off-by: Kui-Feng Lee <kuifeng@fb.com>

--- build.rs
+++ build.rs
@@ -1,13 +1,13 @@
 extern crate cbindgen;
 
+#[cfg(feature= "cheader")]
 use std::env;
+#[cfg(feature= "cheader")]
 use std::path::Path;
 
 fn main() {
-    if env::var("GEN_C_HEADER").is_err() {
-	return;
-    }
-
+    #[cfg(feature= "cheader")]
+    {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let build_type = env::var("PROFILE").unwrap();
     let target_path = Path::new(&crate_dir).join("target").join(build_type);
@@ -17,5 +17,6 @@ fn main() {
 	.with_config(cbindgen::Config::from_root_or_default(&crate_dir))
 	.generate()
 	.expect("Unable to generate bindings")
-	.write_to_file(target_path.join("blazesym.h"));
+	    .write_to_file(target_path.join("blazesym.h"));
+    }
 }
