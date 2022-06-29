f4ccbfa1032a80d3d61facd0a77fd4c2a1779c82
Kui-Feng Lee <kuifeng@fb.com>
2022-06-28 22:14:34 -0700

    Add missing messages in README.md.
    Signed-off-by: Kui-Feng Lee <kuifeng@fb.com>

--- src/lib.rs
+++ src/lib.rs
@@ -436,15 +436,22 @@ pub enum SymbolSrcCfg {
 	/// It should be the address
 	/// in the process mapping to the first byte of the file.
 	/// For example, in /proc/&lt;pid&gt;/maps
+	///
 	/// ```text
 	///     7fe1b2dc4000-7fe1b2f80000 r-xp 00000000 00:1d 71695032                   /usr/lib64/libc-2.28.so
 	///     7fe1b2f80000-7fe1b3180000 ---p 001bc000 00:1d 71695032                   /usr/lib64/libc-2.28.so
 	///     7fe1b3180000-7fe1b3184000 r--p 001bc000 00:1d 71695032                   /usr/lib64/libc-2.28.so
 	///     7fe1b3184000-7fe1b3186000 rw-p 001c0000 00:1d 71695032                   /usr/lib64/libc-2.28.so
 	/// ```
-	/// It shows libc-2.28.so was loaded at 0x7fe1b2dc4000.  This
-	/// address is used to translate an address in a process to the
-	/// address, a relative offset, in the ELF file.
+	///
+	/// It shows the `.text` section of libc-2.28.so was loaded at
+	/// 0x7fe1b2dc4000.  This address is used to translate an
+	/// address in a process to the address, a relative offset, in
+	/// the ELF file.
+	///
+	/// A loader would load a `.text` section with a permission of
+	/// `x`.  For example, the first block is with a permission of
+	/// `r-xp`.
 	base_address: u64,
     },
     /// Linux Kernel's binary image and a copy of /proc/kallsyms
