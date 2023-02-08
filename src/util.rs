adc8efebfbf9517ad4d88c373560fc208c576bca
Daniel Müller <deso@posteo.net>
2023-02-08 15:47:49 -0800

    Introduce ReadRaw trait for easily reading data
    The current practice of reading data and then adjusting some read offset
    based on implicit information is error prone. It leads to unnecessary
    bugs such as https://github.com/libbpf/blazesym/issues/55, where we read
    four bytes and then advance the "read pointer" by two. The existing read
    functions are also infallible, which necessitates length check on each
    and every call site and, if forgotten or done wrong, can be the cause of
    panics. https://github.com/libbpf/blazesym/issues/55 illustrates that as
    well.
    This change introduces a saner API for reading data: the ReadRaw trait.
    Currently implemented for byte slices, it provides fallible methods for
    reading various primitive types and advancing the "read pointer" (the
    start of the slice) accordingly. At this point we implement existing
    decode_* functions in terms of this functionality.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,7 @@
 use std::ffi::CStr;
 use std::fs;
 use std::io::{BufRead, BufReader, Error, ErrorKind};
+use std::mem::size_of;
 use std::path::PathBuf;
 
 use regex::Regex;
@@ -216,38 +217,202 @@ pub fn decode_leb128_s(data: &[u8]) -> Option<(i64, u8)> {
 }
 
 #[inline]
-pub fn decode_uhalf(data: &[u8]) -> u16 {
-    (data[0] as u16) | ((data[1] as u16) << 8)
+pub fn decode_uhalf(mut data: &[u8]) -> u16 {
+    // TODO: Need to handle errors more gracefully.
+    data.read_u16().unwrap()
 }
 
-#[cfg(test)]
 #[inline]
-pub fn decode_shalf(data: &[u8]) -> i16 {
-    let uh = decode_uhalf(data);
-    if uh >= 0x8000 {
-        ((uh as i32) - 0x10000) as i16
-    } else {
-        uh as i16
+pub fn decode_uword(mut data: &[u8]) -> u32 {
+    // TODO: Need to handle errors more gracefully.
+    data.read_u32().unwrap()
+}
+
+#[inline]
+pub fn decode_udword(mut data: &[u8]) -> u64 {
+    // TODO: Need to handle errors more gracefully.
+    data.read_u64().unwrap()
+}
+
+mod sealed {
+    /// A marker trait for "plain old data" data types.
+    ///
+    /// # Safety
+    /// Only safe to implement for types that are valid for any bit pattern.
+    pub unsafe trait Pod {}
+
+    unsafe impl Pod for i8 {}
+    unsafe impl Pod for u8 {}
+    unsafe impl Pod for i16 {}
+    unsafe impl Pod for u16 {}
+    unsafe impl Pod for i32 {}
+    unsafe impl Pod for u32 {}
+    unsafe impl Pod for i64 {}
+    unsafe impl Pod for u64 {}
+    unsafe impl Pod for i128 {}
+    unsafe impl Pod for u128 {}
+}
+
+/// An trait providing utility functions for reading data from a byte buffer.
+pub trait ReadRaw<'data> {
+    /// Ensure that `len` bytes are available for consumption.
+    fn ensure(&self, len: usize) -> Option<()>;
+
+    /// Consume and return `len` bytes.
+    fn read_slice(&mut self, len: usize) -> Option<&'data [u8]>;
+
+    /// Read a NUL terminated string.
+    fn read_cstr(&mut self) -> Option<&'data CStr>;
+
+    /// Read anything implementing `Pod`.
+    #[inline]
+    fn read_pod<T>(&mut self) -> Option<T>
+    where
+        T: sealed::Pod,
+    {
+        let data = self.read_slice(size_of::<T>())?;
+        // SAFETY: `T` is `Pod` and hence valid for any bit pattern. The pointer
+        //         is guaranteed to be valid and to point to memory of at least
+        //         `sizeof(T)` bytes.
+        let value = unsafe { data.as_ptr().cast::<T>().read_unaligned() };
+        Some(value)
+    }
+
+    /// Read a `u8` value.
+    #[inline]
+    fn read_u8(&mut self) -> Option<u8> {
+        self.read_pod::<u8>()
+    }
+
+    /// Read a `i16` value.
+    #[inline]
+    fn read_i16(&mut self) -> Option<i16> {
+        self.read_pod::<i16>()
+    }
+
+    /// Read a `u16` value.
+    #[inline]
+    fn read_u16(&mut self) -> Option<u16> {
+        self.read_pod::<u16>()
+    }
+
+    /// Read a `i32` value.
+    #[inline]
+    fn read_i32(&mut self) -> Option<i32> {
+        self.read_pod::<i32>()
+    }
+
+    /// Read a `u32` value.
+    #[inline]
+    fn read_u32(&mut self) -> Option<u32> {
+        self.read_pod::<u32>()
+    }
+
+    /// Read a `u64` value.
+    #[inline]
+    fn read_u64(&mut self) -> Option<u64> {
+        self.read_pod::<u64>()
     }
 }
 
-#[inline]
-pub fn decode_uword(data: &[u8]) -> u32 {
-    (data[0] as u32) | ((data[1] as u32) << 8) | ((data[2] as u32) << 16) | ((data[3] as u32) << 24)
-}
+impl<'data> ReadRaw<'data> for &'data [u8] {
+    #[inline]
+    fn ensure(&self, len: usize) -> Option<()> {
+        if len > self.len() {
+            return None;
+        }
+        Some(())
+    }
 
-#[cfg(test)]
-#[inline]
-pub fn decode_sword(data: &[u8]) -> i32 {
-    let uw = decode_uword(data);
-    if uw >= 0x80000000 {
-        ((uw as i64) - 0x100000000) as i32
-    } else {
-        uw as i32
+    #[inline]
+    fn read_slice(&mut self, len: usize) -> Option<&'data [u8]> {
+        self.ensure(len)?;
+        let (a, b) = self.split_at(len);
+        *self = b;
+        Some(a)
+    }
+
+    #[inline]
+    fn read_cstr(&mut self) -> Option<&'data CStr> {
+        let idx = self.iter().position(|byte| *byte == b'\0')?;
+        CStr::from_bytes_with_nul(self.read_slice(idx + 1)?).ok()
     }
 }
 
-#[inline]
-pub fn decode_udword(data: &[u8]) -> u64 {
-    decode_uword(data) as u64 | ((decode_uword(&data[4..]) as u64) << 32)
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
+    /// Make sure that `[u8]::ensure` works as expected.
+    #[test]
+    fn u8_slice_len_ensurance() {
+        let slice = [0u8; 0].as_slice();
+        assert_eq!(slice.ensure(0), Some(()));
+        assert_eq!(slice.ensure(1), None);
+
+        let slice = [1u8].as_slice();
+        assert_eq!(slice.ensure(0), Some(()));
+        assert_eq!(slice.ensure(1), Some(()));
+        assert_eq!(slice.ensure(2), None);
+    }
+
+    /// Check that we can read various integers from a slice.
+    #[test]
+    fn pod_reading() {
+        macro_rules! test {
+            ($type:ty) => {{
+                let max = <$type>::MAX.to_ne_bytes();
+                let one = (1 as $type).to_ne_bytes();
+
+                let mut data = Vec::new();
+                let () = data.extend_from_slice(&max);
+                let () = data.extend_from_slice(&one);
+                let () = data.extend_from_slice(&[1, 2, 3, 4, 5, 6, 7, 8]);
+
+                let mut raw = data.as_slice();
+                let uword = raw.read_pod::<$type>().unwrap();
+                assert_eq!(uword, <$type>::MAX);
+
+                let uword = raw.read_pod::<$type>().unwrap();
+                assert_eq!(uword, 1);
+            }};
+        }
+
+        test!(i8);
+        test!(u8);
+        test!(i16);
+        test!(u16);
+        test!(i32);
+        test!(u32);
+        test!(i64);
+        test!(u64);
+        test!(i128);
+        test!(u128);
+    }
+
+    /// Test reading of signed and unsigned 16 and 32 bit values against known
+    /// results.
+    #[test]
+    fn word_reading() {
+        let data = 0xf936857fu32.to_ne_bytes();
+        assert_eq!(data.as_slice().read_u16().unwrap(), 0x857f);
+        assert_eq!(data.as_slice().read_i16().unwrap(), -31361);
+        assert_eq!(data.as_slice().read_u32().unwrap(), 0xf936857f);
+        assert_eq!(data.as_slice().read_i32().unwrap(), -113867393);
+    }
+
+    /// Check that we can read a NUL terminated string from a slice.
+    #[test]
+    fn cstr_reading() {
+        let mut slice = b"abc\x001234".as_slice();
+
+        let cstr = slice.read_cstr().unwrap();
+        assert_eq!(cstr, CStr::from_bytes_with_nul(b"abc\0").unwrap());
+
+        // No terminating NUL byte.
+        let mut slice = b"abc".as_slice();
+        assert_eq!(slice.read_cstr(), None);
+    }
 }
