243dfa79564948b54b9680e4841fa12d39c3ed61
Daniel Müller <deso@posteo.net>
2023-03-10 15:05:53 -0800

    Include blazesym-benchmarks into repository
    I originally created blazesym-benchmarks [0] as a separate repository
    because we wanted to include and benchmark on "real world" data, which
    was rather big. But given that the data compresses rather well (700 MiB
    image down to ~100 MiB file) and that we now use git-lfs to manage this
    data outside of the repository proper and that checkout of it is
    optional, it arguably makes more sense to include these benchmarks here
    right away instead. Doing so has the advantage that benchmarks are
    co-located with library code in a single repository.
    In order to run this new set of benchmarks the generate-bench-files
    feature must be present. The benchmarks take a while to run and are
    generally run much less frequent than tests, so an opt-in scheme seems
    much more apt than what we have for tests where we generate test files
    by default in dev-builds. That's also done to not *require* users to
    have git-lfs installed.
    We use criterion as opposed to Rust's built-in benchmark here. The
    former provide better configurability and more rigorous statistical
    analysis. The downside is that integration is not quite as nice (with a
    bunch of macro magic), but it is function over form in this instance.
    
    [0] https://github.com/danielocfb/blazesym-benchmarks
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

---
+++ benches/main.rs
@@ -0,0 +1,25 @@
+mod dwarf;
+
+use std::time::Duration;
+
+use criterion::criterion_group;
+use criterion::criterion_main;
+use criterion::Criterion;
+
+
+fn benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("dwarf");
+    group.sample_size(500);
+    group.warm_up_time(Duration::from_secs(5));
+    group.confidence_level(0.98);
+    group.significance_level(0.02);
+    group.bench_function(stringify!(dwarf::lookup_end_to_end), |b| {
+        b.iter(dwarf::lookup_end_to_end)
+    });
+    group.bench_function(stringify!(dwarf::symbolize_end_to_end), |b| {
+        b.iter(dwarf::symbolize_end_to_end)
+    });
+}
+
+criterion_group!(benches, benchmark);
+criterion_main!(benches);
