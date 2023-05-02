c83122b649cfed0a2592d58b08d18a01a3c776b0
Daniel Müller <deso@posteo.net>
2023-05-02 11:35:13 -0700

    Move blazesym_find_addrs() into inspect module
    This change moves the blazesym_find_addrs() function along with a bunch
    of type definitions into the newly created 'inspect' sub-module.
    Upcoming changes will adapt it to work with the inspector type instead
    of on a symbolizer.
    I renamed functions & types in the process of the move to reflect new
    naming (e.g., blaze_ prefix as opposed to blazesym_, shorter names with
    less duplication, etc.).
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- src/c_api/mod.rs
+++ src/c_api/mod.rs
@@ -1,7 +1,10 @@
 #[allow(non_camel_case_types)]
+mod inspect;
+#[allow(non_camel_case_types)]
 mod normalize;
 #[allow(non_camel_case_types)]
 mod symbolize;
 
+pub use inspect::*;
 pub use normalize::*;
 pub use symbolize::*;
