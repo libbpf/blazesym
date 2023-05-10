9c0c163c8cea447ad3c11bc6b1a119f59240d017
Daniel Müller <deso@posteo.net>
2023-05-10 14:23:51 -0700

    Refactor lookup logic in Inspector
    With commit a03c15efddab ("Remove 'symbolize' feature") we simplified
    the conditional compilation logic and that enables us to expose the
    ElfBackend enum from the elf module without any conflicts/potentially
    unresolved dependencies.
    With this change we make use of that fact, by reworking the Inspector
    internal find_addr logic to use an ElfResolver on all paths. Doing so
    will make follow on changes more straight forward.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- src/elf/mod.rs
+++ src/elf/mod.rs
@@ -4,6 +4,7 @@ mod resolver;
 #[allow(non_camel_case_types)]
 pub(crate) mod types;
 
+pub(crate) use cache::ElfBackend;
 pub(crate) use cache::ElfCache;
 pub(crate) use parser::ElfParser;
 pub(crate) use resolver::ElfResolver;
