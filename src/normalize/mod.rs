31bd198a19939993e269d743845766e686923d85
Daniel Müller <deso@posteo.net>
2023-06-05 15:16:00 -0700

    Improve normalization example
    Make it a bit clearer what the normalization module does by improving
    the code example with a few more details.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- src/normalize/mod.rs
+++ src/normalize/mod.rs
@@ -1,6 +1,6 @@
 //! Functionality for address normalization.
 //!
-//! Address normalization is one step of address symbolization: before can
+//! Address normalization is one step of address symbolization: before an
 //! address as captured in a process can be looked up in a file, it needs to be
 //! converted into an address as it will be used inside the file. E.g.,
 //! addresses in a shared object (or any position independent binary) may be
@@ -11,12 +11,19 @@
 //!
 //! ```no_run
 //! use blazesym::normalize::Normalizer;
+//! use blazesym::Addr;
 //! use blazesym::Pid;
 //!
 //! let normalizer = Normalizer::new();
-//! let addrs = [0xdeadbeef];
-//! let pid = Pid::from(1234);
+//! let fopen_addr = libc::fopen as Addr;
+//! let addrs = [fopen_addr];
+//! let pid = Pid::Slf;
 //! let norm_addrs = normalizer.normalize_user_addrs(&addrs, pid).unwrap();
+//! assert_eq!(norm_addrs.addrs.len(), 1);
+//!
+//! let (addr, meta_idx) = norm_addrs.addrs[0];
+//! // fopen (0x7f5f8e23a790) is at address 0x77790 in Binary(Binary { path: "/usr/lib64/libc.so.6", build_id: Some([...]), ... })
+//! println!("fopen (0x{fopen_addr:x}) is at address 0x{addr:x} in {:?}", norm_addrs.meta[meta_idx]);
 //! ```
 
 mod meta;
