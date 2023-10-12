89c8f91058110090a9bf0f0bd93157b23bf58e3c
Daniel Müller <deso@posteo.net>
2023-10-12 15:03:02 -0700

    Rename and move normalize_apk_offset()
    The normalize_apk_offset() function is only used by the symbolization
    logic now. Move it and rename it to be less confusing given this
    context.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- src/normalize/mod.rs
+++ src/normalize/mod.rs
@@ -44,7 +44,6 @@ pub use normalizer::Normalizer;
 pub use user::UserOutput;
 
 pub(crate) use user::create_apk_elf_path;
-pub(crate) use user::normalize_apk_offset;
 pub(crate) use user::normalize_elf_addr;
 pub(crate) use user::normalize_elf_offset_with_parser;
 pub(crate) use user::normalize_sorted_user_addrs_with_entries;
