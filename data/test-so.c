849a741486cc61cf52a8d05cc39e17898f1ab5e7
Daniel Müller <deso@posteo.net>
2023-12-18 13:50:24 -0800

    Add test for dynamic symbol lookup & symbolization
    This change adds a test for the lookup of a dynamic symbol by name as
    well as by address.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- data/test-so.c
+++ data/test-so.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 
+#include "test-so.h"
+
 int the_answer(void) {
   return 42;
+}
+
+int the_ignored_answer(void) {
+  return 43;
 }
 
 int await_input(void) {
