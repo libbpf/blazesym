1806891a6f269dc87b13549961554888a5838794
Daniel Müller <deso@posteo.net>
2024-01-19 10:17:33 -0800

    Introduce Pid::resolve() helper
    With an upcoming change we will need to be able to "resolve" the
    symbolic process ID Pid::Slf to the actual number. This change adds a
    helper method, Pid::resolve(), that accomplishes this feat.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- src/pid.rs
+++ src/pid.rs
@@ -2,6 +2,7 @@ use std::fmt::Display;
 use std::fmt::Formatter;
 use std::fmt::Result as FmtResult;
 use std::num::NonZeroU32;
+use std::process;
 
 
 /// An enumeration identifying a process.
@@ -11,6 +12,17 @@ pub enum Pid {
     Slf,
     /// The process identified by the provided ID.
     Pid(NonZeroU32),
+}
+
+impl Pid {
+    /// Resolve this [`Pid`] into an actual number, if it is the symbolic
+    /// [`Pid::Slf`] variant.
+    pub(crate) fn resolve(&self) -> u32 {
+        match self {
+            Self::Slf => process::id(),
+            Self::Pid(pid) => pid.get(),
+        }
+    }
 }
 
 impl Display for Pid {
@@ -25,5 +37,37 @@ impl Display for Pid {
 impl From<u32> for Pid {
     fn from(pid: u32) -> Self {
         NonZeroU32::new(pid).map(Pid::Pid).unwrap_or(Pid::Slf)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::fs::read_link;
+
+
+    fn resolve_pid() -> u32 {
+        let path = read_link("/proc/self").unwrap();
+        let file = path.file_name().unwrap();
+        let name = file.to_str().unwrap();
+        let pid = name.parse::<u32>().unwrap();
+        pid
+    }
+
+
+    /// Exercise the `Debug` representation of various types.
+    #[test]
+    fn debug_repr() {
+        let pid = Pid::Slf;
+        assert_ne!(format!("{pid:?}"), "");
+    }
+
+    /// Check that we can resolve a symbolic PID name.
+    #[test]
+    fn pid_resolution() {
+        let pid = Pid::Slf.resolve();
+        assert_eq!(pid, resolve_pid());
     }
 }
