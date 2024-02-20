1cae4017f62a26205679ca1a44d20fe6b6a23377
Daniel Müller <deso@posteo.net>
2024-02-20 10:50:32 -0800

    Add benchmark for normalizing without reading of build IDs
    Add a benchmark for normalizing a set of addresses without reading of
    build IDs. This way we can easily assess the impact that reading of
    build IDs has on normalization performance.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- benches/normalize.rs
+++ benches/normalize.rs
@@ -9,8 +9,7 @@ use criterion::measurement::Measurement;
 use criterion::BenchmarkGroup;
 
 
-/// Normalize addresses in the current process.
-fn normalize_process() {
+fn normalize_process_impl(read_build_ids: bool) {
     let mut addrs = [
         libc::__errno_location as Addr,
         libc::dlopen as Addr,
@@ -20,11 +19,25 @@ fn normalize_process() {
     ];
     let () = addrs.sort();
 
-    let normalizer = Normalizer::new();
+    let normalizer = Normalizer::builder()
+        .enable_build_ids(read_build_ids)
+        .build();
     let normalized = normalizer
         .normalize_user_addrs_sorted(black_box(0.into()), black_box(addrs.as_slice()))
         .unwrap();
     assert_eq!(normalized.outputs.len(), 5);
+}
+
+
+/// Normalize addresses in the current process, and read build IDs as part of
+/// the normalization.
+fn normalize_process() {
+    normalize_process_impl(true)
+}
+
+/// Normalize addresses in the current process, but don't read build IDs.
+fn normalize_process_no_build_ids() {
+    normalize_process_impl(false)
 }
 
 pub fn benchmark<M>(group: &mut BenchmarkGroup<'_, M>)
@@ -32,4 +45,5 @@ where
     M: Measurement,
 {
     bench_fn!(group, normalize_process);
+    bench_fn!(group, normalize_process_no_build_ids);
 }
