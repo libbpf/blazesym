9518be4f986c4f4597eb8bb557ba1865db33f94b
Daniel Müller <deso@posteo.net>
2024-02-27 14:29:02 -0800

    Add infrastructure for capturing allocation statistics
    This change adds some infrastructure that we can use for capturing
    memory allocation statistics. As a first step we capture such statistics
    for the address normalization path. We include the means for printing
    backtraces for each allocation being made, which can help with better
    understanding the reported numbers.
    
      > $ cargo test --test=allocs -- normalize_process --nocapture
      > Stats: Stats {
      >   allocations: 47,
      >   deallocations: 47,
      >   reallocations: 11,
      >   bytes_allocated: 19742,
      >   bytes_deallocated: 19742,
      >   bytes_reallocated: 540,
      > }
      > $ RUST_BACKTRACE=1 cargo test --test=allocs -- normalize_process --nocapture
      > ...
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

---
+++ tests/allocs.rs
@@ -0,0 +1,85 @@
+#![allow(
+    clippy::fn_to_numeric_cast,
+    clippy::let_and_return,
+    clippy::let_unit_value
+)]
+
+use std::alloc::GlobalAlloc;
+use std::alloc::Layout;
+use std::alloc::System;
+use std::backtrace::Backtrace;
+use std::backtrace::BacktraceStatus;
+use std::hint::black_box;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
+use std::thread_local;
+
+use blazesym::normalize::Normalizer;
+use blazesym::Addr;
+
+use stats_alloc::Region;
+use stats_alloc::StatsAlloc;
+
+#[global_allocator]
+static GLOBAL: StatsAlloc<TracingAlloc> = StatsAlloc::new(TracingAlloc);
+
+thread_local! {
+  static TRACING: AtomicBool = AtomicBool::new(false);
+}
+
+
+/// An allocator that prints a backtrace for each allocation being made.
+struct TracingAlloc;
+
+unsafe impl GlobalAlloc for TracingAlloc {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        // Capturing a backtrace will allocate itself. Prevent infinite
+        // recursion with a flag.
+        if !TRACING.with(|tracing| tracing.swap(true, Ordering::Relaxed)) {
+            let bt = Backtrace::capture();
+            if let BacktraceStatus::Captured = bt.status() {
+                println!("{layout:?}:\n{bt}");
+            }
+            let () = TRACING.with(|tracing| tracing.store(false, Ordering::Relaxed));
+        }
+        System.alloc(layout)
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        System.dealloc(ptr, layout);
+    }
+}
+
+
+/// Normalize addresses in the current process and print allocation
+/// statistics.
+#[test]
+fn normalize_process() {
+    let region = Region::new(&GLOBAL);
+
+    {
+        let normalizer = Normalizer::builder().build();
+        let mut addrs = [
+            libc::__errno_location as Addr,
+            libc::dlopen as Addr,
+            libc::fopen as Addr,
+            normalize_process as Addr,
+            Normalizer::normalize_user_addrs_sorted as Addr,
+        ];
+        let () = addrs.sort();
+
+        let normalized = normalizer
+            .normalize_user_addrs_sorted(black_box(0.into()), black_box(addrs.as_slice()))
+            .unwrap();
+        assert_eq!(normalized.meta.len(), 2);
+        assert_eq!(normalized.outputs.len(), 5);
+    }
+
+    // We can't make many assumptions about the allocations here,
+    // because a lot of it is system dependent. E.g., more entries in
+    // `/proc/<pid>/maps` likely means more allocations. Even the order
+    // may have an influence, to the point that ASLR could change
+    // results.
+    let stats = region.change();
+    println!("Stats: {stats:#?}");
+}
