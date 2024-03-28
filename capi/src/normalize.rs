2c018171c969bdf9b7f64c6d6b8d3db61e834229
Daniel Müller <deso@posteo.net>
2024-03-28 13:22:37 -0700

    capi: Adjust blaze_normalize_user_addrs*() to set last error
    Adjust the blaze_normalize_user_addrs*() set of functions to set the
    last error, providing a more detailed categorization of errors to users.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- capi/src/normalize.rs
+++ capi/src/normalize.rs
@@ -493,19 +493,27 @@ impl blaze_normalized_user_output {
 
 /// Normalize a list of user space addresses.
 ///
+/// C ABI compatible version of [`Normalizer::normalize_user_addrs`].
+///
+/// `pid` should describe the PID of the process to which the addresses
+/// belongs. It may be `0` if they belong to the calling process.
+///
+/// On success, the function creates a new [`blaze_normalized_user_output`]
+/// object and returns it. The resulting object should be released using
+/// [`blaze_user_output_free`] once it is no longer needed.
+///
+/// On error, the function returns `NULL` and sets the thread's last error to
+/// indicate the problem encountered. Use [`blaze_err_last`] to retrieve this
+/// error.
+///
 /// Contrary to [`blaze_normalize_user_addrs_sorted`] the provided
 /// `addrs` array does not have to be sorted, but otherwise the
 /// functions behave identically. If you happen to know that `addrs` is
 /// sorted, using [`blaze_normalize_user_addrs_sorted`] instead will
 /// result in slightly faster normalization.
 ///
-/// C ABI compatible version of [`Normalizer::normalize_user_addrs`].
-/// Returns `NULL` on error. The resulting object should be freed using
-/// [`blaze_user_output_free`].
-///
 /// # Safety
-/// Callers need to pass in a valid `addrs` pointer, pointing to memory of
-/// `addr_cnt` addresses.
+/// - `addrs` needs to be a valid pointer to `addr_cnt` addresses
 #[no_mangle]
 pub unsafe extern "C" fn blaze_normalize_user_addrs(
     normalizer: *const blaze_normalizer,
@@ -521,31 +529,43 @@ pub unsafe extern "C" fn blaze_normalize_user_addrs(
     let addrs = unsafe { slice_from_user_array(addrs, addr_cnt) };
     let result = normalizer.normalize_user_addrs(pid.into(), addrs);
     match result {
-        Ok(addrs) => Box::into_raw(Box::new(ManuallyDrop::into_inner(
-            blaze_normalized_user_output::from(addrs),
-        ))),
-        Err(_err) => ptr::null_mut(),
+        Ok(addrs) => {
+            let output_box = Box::new(ManuallyDrop::into_inner(
+                blaze_normalized_user_output::from(addrs),
+            ));
+            let () = set_last_err(blaze_err::BLAZE_ERR_OK);
+            Box::into_raw(output_box)
+        }
+        Err(err) => {
+            let () = set_last_err(err.kind().into());
+            ptr::null_mut()
+        }
     }
 }
 
 
 /// Normalize a list of user space addresses.
 ///
+/// C ABI compatible version of [`Normalizer::normalize_user_addrs_sorted`].
+///
+/// `pid` should describe the PID of the process to which the addresses
+/// belongs. It may be `0` if they belong to the calling process.
+///
 /// The `addrs` array has to be sorted in ascending order. By providing
 /// a pre-sorted array the library does not have to sort internally,
 /// which will result in quicker normalization. If you don't have sorted
 /// addresses, use [`blaze_normalize_user_addrs`] instead.
 ///
-/// `pid` should describe the PID of the process to which the addresses
-/// belongs. It may be `0` if they belong to the calling process.
+/// On success, the function creates a new [`blaze_normalized_user_output`]
+/// object and returns it. The resulting object should be released using
+/// [`blaze_user_output_free`] once it is no longer needed.
 ///
-/// C ABI compatible version of [`Normalizer::normalize_user_addrs_sorted`].
-/// Returns `NULL` on error. The resulting object should be freed using
-/// [`blaze_user_output_free`].
+/// On error, the function returns `NULL` and sets the thread's last error to
+/// indicate the problem encountered. Use [`blaze_err_last`] to retrieve this
+/// error.
 ///
 /// # Safety
-/// Callers need to pass in a valid `addrs` pointer, pointing to memory of
-/// `addr_cnt` addresses.
+/// - `addrs` needs to be a valid pointer to `addr_cnt` addresses
 #[no_mangle]
 pub unsafe extern "C" fn blaze_normalize_user_addrs_sorted(
     normalizer: *const blaze_normalizer,
@@ -561,12 +581,20 @@ pub unsafe extern "C" fn blaze_normalize_user_addrs_sorted(
     let addrs = unsafe { slice_from_user_array(addrs, addr_cnt) };
     let result = normalizer.normalize_user_addrs_sorted(pid.into(), addrs);
     match result {
-        Ok(addrs) => Box::into_raw(Box::new(ManuallyDrop::into_inner(
-            blaze_normalized_user_output::from(addrs),
-        ))),
-        Err(_err) => ptr::null_mut(),
+        Ok(addrs) => {
+            let output_box = Box::new(ManuallyDrop::into_inner(
+                blaze_normalized_user_output::from(addrs),
+            ));
+            let () = set_last_err(blaze_err::BLAZE_ERR_OK);
+            Box::into_raw(output_box)
+        }
+        Err(err) => {
+            let () = set_last_err(err.kind().into());
+            ptr::null_mut()
+        }
     }
 }
+
 
 /// Free an object as returned by [`blaze_normalize_user_addrs`] or
 /// [`blaze_normalize_user_addrs_sorted`].
