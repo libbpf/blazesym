f9020076366592820a85f634bdd440d3a000eb6b
Daniel Müller <deso@posteo.net>
2024-04-04 15:01:20 -0700

    cli: Add support for Breakpad files to 'inspect' command
    Add support for working with Breakpad files to the 'inspect' command.
    
    $ cargo run --package blazecli -- inspect lookup breakpad --path data/test-stable-addresses.sym resolve_indirect_func main i_exist_twice
    > resolve_indirect_func: 0x00000000000088+11   [FUNC]
    > i_exist_twice:         0x00000000000040+14   [FUNC]
    > i_exist_twice:         0x00000000000093+14   [FUNC]
    
    $ cargo run --package blazecli -- inspect dump breakpad --path data/test-stable-addresses.sym
    > i_exist_twice:         0x00000000000040+14   [FUNC]
    > factorial_wrapper:     0x0000000000004e+17   [FUNC]
    > foo:                   0x0000000000005f+17   [FUNC]
    > factorial_wrapper:     0x00000000000070+17   [FUNC]
    > my_indirect_func:      0x00000000000081+7    [FUNC]
    > resolve_indirect_func: 0x00000000000088+11   [FUNC]
    > i_exist_twice:         0x00000000000093+14   [FUNC]
    > factorial:             0x00000000000100+43   [FUNC]
    > factorial_inline_test: 0x00000000000200+19   [FUNC]
    
    Refs: #304
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- data/test-stable-addresses-cu2.c
+++ data/test-stable-addresses-cu2.c
@@ -1,15 +1,16 @@
 extern unsigned int factorial(unsigned int n);
 extern volatile char a_variable[8];
 
-__attribute__((noinline)) static void i_exist_twice(void) {
-  a_variable[0] = '\0';
-}
-
 __attribute__((noinline)) static void
 factorial_wrapper() {
 	factorial(5);
 }
 
+__attribute__((noinline)) static void i_exist_twice(void) {
+  a_variable[0] = '\0';
+}
+
 void foo(void) {
 	factorial_wrapper();
+  i_exist_twice();
 }
