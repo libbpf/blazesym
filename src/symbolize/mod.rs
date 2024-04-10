15931db58ebc92c09fbc3bc2334e3b2d85ae9350
Daniel Müller <deso@posteo.net>
2024-04-10 16:14:09 -0700

    Introduce FindSymOpts helper type
    With an upcoming change we are going to require a counter part of the
    existing FindAddrOpts type that is used internally for driving the
    inspect APIs for the symbolization APIs. To that end, this change
    introduces the FindSymOpts enum, which determines what information to
    look up about a symbol.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- src/symbolize/mod.rs
+++ src/symbolize/mod.rs
@@ -129,6 +129,38 @@ use crate::normalize;
 use crate::Addr;
 
 
+/// Options determining what "parts" of a symbol to look up.
+#[derive(Debug)]
+pub(crate) enum FindSymOpts {
+    /// Only look up the "basic" symbol data (name, address, size, ...), without
+    /// source code location and inlined function information.
+    Basic,
+    /// Look up symbol data and source code location information.
+    CodeInfo,
+    /// Look up symbol data, source code location information, and inlined
+    /// function information.
+    CodeInfoAndInlined,
+}
+
+impl FindSymOpts {
+    #[inline]
+    pub(crate) fn code_info(&self) -> bool {
+        match self {
+            Self::Basic => false,
+            Self::CodeInfo | Self::CodeInfoAndInlined => true,
+        }
+    }
+
+    #[inline]
+    pub(crate) fn inlined_fns(&self) -> bool {
+        match self {
+            Self::Basic | Self::CodeInfo => false,
+            Self::CodeInfoAndInlined => true,
+        }
+    }
+}
+
+
 /// A enumeration of the different input types the symbolization APIs
 /// support.
 #[derive(Clone, Copy, Debug)]
