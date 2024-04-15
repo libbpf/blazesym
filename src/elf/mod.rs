cf35ef6adb10b6d03ddf7fbd627b09d40f6ef5cd
Daniel Müller <deso@posteo.net>
2024-04-15 16:24:49 -0700

    Make ElfBackend an implementation detail of elf::resolver module
    Move the ElfBackend enum into elf::resolver, which really is the only
    module using it. Having it isolated there means that it's unlikely to
    permeate into other parts of the library.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- src/elf/mod.rs
+++ src/elf/mod.rs
@@ -1,11 +1,9 @@
-mod backend;
 mod parser;
 #[cfg_attr(not(feature = "dwarf"), allow(unused_variables))]
 mod resolver;
 #[allow(dead_code, non_camel_case_types)]
 pub(crate) mod types;
 
-pub(crate) use backend::ElfBackend;
 pub(crate) use parser::ElfParser;
 pub(crate) use resolver::ElfResolver;
 pub(crate) use resolver::ElfResolverData;
