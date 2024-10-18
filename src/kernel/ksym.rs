2f0529dd0808b0f9202a10aa6b0a1b2eac71f370
Daniel Müller <deso@posteo.net>
2024-10-18 08:18:15 -0700

    Add support for symbolizing BPF program kernel addresses
    This change adds the remaining plumbing for symbolizing BPF program
    kernel addresses. When a kernel address falls into a BPF program, we
    query all the necessary information to see if the kernel is able to
    provide us with source code information about said address and furnish
    up the corresponding CodeInfo object to include it in the symbolization
    result.
    
    Closes: #826
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- src/kernel/ksym.rs
+++ src/kernel/ksym.rs
@@ -29,7 +29,12 @@ use crate::Result;
 use crate::SymType;
 
 #[cfg(feature = "bpf")]
+use super::bpf::BpfInfoCache;
+#[cfg(feature = "bpf")]
 use super::bpf::BpfProg;
+
+#[cfg(not(feature = "bpf"))]
+type BpfInfoCache = ();
 
 pub const KALLSYMS: &str = "/proc/kallsyms";
 const DFL_KSYM_CAP: usize = 200000;
@@ -56,11 +61,16 @@ impl Ksym {
         })
     }
 
-    fn resolve(&self, addr: Addr, opts: &FindSymOpts) -> Result<ResolvedSym<'_>> {
+    fn resolve(
+        &self,
+        addr: Addr,
+        opts: &FindSymOpts,
+        _bpf_info_cache: &BpfInfoCache,
+    ) -> Result<ResolvedSym<'_>> {
         match self {
             Ksym::Kfunc(kfunc) => kfunc.resolve(addr, opts),
             #[cfg(feature = "bpf")]
-            Ksym::BpfProg(bpf_prog) => bpf_prog.resolve(addr, opts),
+            Ksym::BpfProg(bpf_prog) => bpf_prog.resolve(addr, opts, _bpf_info_cache),
         }
     }
 
@@ -164,6 +174,7 @@ pub(crate) struct KSymResolver {
     by_name_idx: OnceCell<Box<[usize]>>,
     syms: Box<[Ksym]>,
     file_name: PathBuf,
+    bpf_info_cache: BpfInfoCache,
 }
 
 impl KSymResolver {
@@ -214,6 +225,7 @@ impl KSymResolver {
             syms: syms.into_boxed_slice(),
             by_name_idx: OnceCell::new(),
             file_name: path.to_path_buf(),
+            bpf_info_cache: BpfInfoCache::default(),
         };
         Ok(slf)
     }
@@ -231,6 +243,7 @@ impl KSymResolver {
                 .into_boxed_slice(),
             by_name_idx: OnceCell::new(),
             file_name: PathBuf::new(),
+            bpf_info_cache: BpfInfoCache::default(),
         }
     }
 
@@ -272,7 +285,7 @@ impl Symbolize for KSymResolver {
     fn find_sym(&self, addr: Addr, opts: &FindSymOpts) -> Result<Result<ResolvedSym<'_>, Reason>> {
         match self.find_ksym(addr) {
             Ok(ksym) => {
-                let sym = ksym.resolve(addr, opts)?;
+                let sym = ksym.resolve(addr, opts, &self.bpf_info_cache)?;
                 Ok(Ok(sym))
             }
             Err(reason) => Ok(Err(reason)),
@@ -355,6 +368,7 @@ mod tests {
             syms: Box::default(),
             by_name_idx: OnceCell::new(),
             file_name: PathBuf::new(),
+            bpf_info_cache: BpfInfoCache::default(),
         };
         assert_ne!(format!("{resolver:?}"), "");
 
