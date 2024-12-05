9425bdc4d4da4c1c37620d093b89f5660bdee2cf
Daniel Müller <deso@posteo.net>
2024-12-05 12:57:21 -0800

    Add support for working with Elf32_Nhdr objects
    Add the necessary logic for working with 32 bit note headers.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- src/normalize/buildid.rs
+++ src/normalize/buildid.rs
@@ -2,6 +2,7 @@ use std::borrow::Cow;
 use std::path::Path;
 
 use crate::elf;
+use crate::elf::types::Elf32_Nhdr;
 use crate::elf::types::Elf64_Nhdr;
 use crate::elf::ElfParser;
 use crate::file_cache::FileCache;
@@ -26,17 +27,26 @@ fn read_build_id_from_notes(parser: &ElfParser) -> Result<Option<BuildId<'_>>> {
             // SANITY: We just found the index so the section data should always
             //         be found.
             let mut bytes = parser.section_data(idx).unwrap();
-            let header = bytes
-                .read_pod_ref::<Elf64_Nhdr>()
-                .ok_or_invalid_data(|| "failed to read build ID section header")?;
-            if header.n_type == elf::types::NT_GNU_BUILD_ID {
+            let (n_type, n_namesz, n_descsz) = if shdr.is_32bit() {
+                let nhdr = bytes
+                    .read_pod_ref::<Elf32_Nhdr>()
+                    .ok_or_invalid_data(|| "failed to read build ID section header")?;
+                (nhdr.n_type, nhdr.n_namesz, nhdr.n_descsz)
+            } else {
+                let nhdr = bytes
+                    .read_pod_ref::<Elf64_Nhdr>()
+                    .ok_or_invalid_data(|| "failed to read build ID section header")?;
+                (nhdr.n_type, nhdr.n_namesz, nhdr.n_descsz)
+            };
+
+            if n_type == elf::types::NT_GNU_BUILD_ID {
                 // Type check is assumed to suffice, but we still need
                 // to skip the name bytes.
                 let _name = bytes
-                    .read_slice(header.n_namesz as _)
+                    .read_slice(n_namesz as _)
                     .ok_or_invalid_data(|| "failed to read build ID section name")?;
                 let build_id = bytes
-                    .read_slice(header.n_descsz as _)
+                    .read_slice(n_descsz as _)
                     .ok_or_invalid_data(|| "failed to read build ID section contents")?;
                 return Ok(Some(Cow::Borrowed(build_id)))
             }
@@ -65,11 +75,20 @@ fn read_build_id_from_section_name(parser: &ElfParser) -> Result<Option<BuildId<
         // SANITY: We just found the index so the section should always be
         //         found.
         let mut bytes = parser.section_data(idx).unwrap();
-        let header = bytes
-            .read_pod_ref::<Elf64_Nhdr>()
-            .ok_or_invalid_data(|| "failed to read build ID section header")?;
+        let (n_namesz, n_descsz) = if shdr.is_32bit() {
+            let nhdr = bytes
+                .read_pod_ref::<Elf32_Nhdr>()
+                .ok_or_invalid_data(|| "failed to read build ID section header")?;
+            (nhdr.n_namesz, nhdr.n_descsz)
+        } else {
+            let nhdr = bytes
+                .read_pod_ref::<Elf64_Nhdr>()
+                .ok_or_invalid_data(|| "failed to read build ID section header")?;
+            (nhdr.n_namesz, nhdr.n_descsz)
+        };
+
         let name = bytes
-            .read_slice(header.n_namesz as _)
+            .read_slice(n_namesz as _)
             .and_then(|mut name| name.read_cstr())
             .ok_or_invalid_data(|| "failed to read build ID section name")?;
         if name.to_bytes() != b"GNU" {
@@ -77,7 +96,7 @@ fn read_build_id_from_section_name(parser: &ElfParser) -> Result<Option<BuildId<
             Ok(None)
         } else {
             let build_id = bytes
-                .read_slice(header.n_descsz as _)
+                .read_slice(n_descsz as _)
                 .ok_or_invalid_data(|| "failed to read build ID section contents")?;
             Ok(Some(Cow::Borrowed(build_id)))
         }
