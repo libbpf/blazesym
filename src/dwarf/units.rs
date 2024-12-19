2009a1de670c46e986e9e23e3b260ea07464cd50
Daniel Müller <deso@posteo.net>
2024-12-19 10:27:30 -0800

    Allow clippy::type_complexity lint library-wide
    Allow the clippy::type_complexity lint in a library-wide fashion. It's a
    subjective heuristic and more often than not the introduction of a
    typedef or similar doesn't aid at all in readability.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- src/dwarf/units.rs
+++ src/dwarf/units.rs
@@ -341,7 +341,6 @@ impl<'dwarf> Units<'dwarf> {
     }
 
     /// Find the list of inlined functions that contain `probe`.
-    #[allow(clippy::type_complexity)]
     pub(super) fn find_inlined_functions<'slf>(
         &'slf self,
         probe: u64,
