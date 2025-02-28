3725107ec0f2e4a4de9ef6e89297f66aee659191
Daniel Müller <deso@posteo.net>
2025-02-28 10:30:47 -0800

    Add missing closing parenthesis in documentation
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- capi/src/normalize.rs
+++ capi/src/normalize.rs
@@ -326,7 +326,7 @@ impl blaze_user_meta_apk {
 pub struct blaze_user_meta_elf {
     /// Ordinarily, the canonical absolute path to the ELF file,
     /// including its name. In case of an ELF file contained inside an
-    /// APK (see [`blaze_normalize_opts::apk_to_elf`] this will be an
+    /// APK (see [`blaze_normalize_opts::apk_to_elf`]) this will be an
     /// Android style path of the form `<apk>!<elf-in-apk>`. E.g.,
     /// `/root/test.apk!/lib/libc.so`.
     ///
