be745c09e5012838401299e15224d36000206659
Daniel Müller <deso@posteo.net>
2025-08-15 15:15:48 -0700

    Introduce load_section_impl() helper function
    Introduce the load_section_impl() helper function, which may come in
    handy down the line when we want to load section's by something other
    than their "direct" name.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- src/dwarf/reader.rs
+++ src/dwarf/reader.rs
@@ -15,14 +15,22 @@ type Endianess = gimli::BigEndian;
 pub(crate) type R<'dat> = EndianSlice<'dat, Endianess>;
 
 
-pub(super) fn load_section(parser: &ElfParser, id: SectionId) -> Result<R<'_>> {
-    let result = parser.find_section(id.name())?;
-    let data = match result {
-        Some(idx) => parser.section_data(idx)?,
-        // Make sure to return empty data if a section does not exist.
-        None => &[],
+fn load_section_impl<'elf>(parser: &'elf ElfParser, name: Option<&str>) -> Result<R<'elf>> {
+    let data = if let Some(name) = name {
+        let result = parser.find_section(name)?;
+        match result {
+            Some(idx) => parser.section_data(idx)?,
+            // Make sure to return empty data if a section does not exist.
+            None => &[],
+        }
+    } else {
+        &[]
     };
 
     let reader = EndianSlice::new(data, Endianess::default());
     Ok(reader)
+}
+
+pub(super) fn load_section(parser: &ElfParser, id: SectionId) -> Result<R<'_>> {
+    load_section_impl(parser, Some(id.name()))
 }
