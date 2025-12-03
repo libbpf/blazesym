f8eb7274c5db66d5dfa34ead6dc8942b2dbd25de
Daniel Müller <deso@posteo.net>
2025-12-03 14:25:37 -0800

    Add ELF file cache to KernelCache
    Add an ELF file cache to the KernelCache type. With the KernelCache
    taking on the role of caching all things related to kernel data, it
    seems sensible to stop piggy-backing on the Symbolizer's ELF cache for
    files such as the vmlinux image (and, in the future, kernel modules) and
    instead maintain a separate entity. We could share the cache between
    both entities for minor memory savings, but that comes with an increased
    complexity cost.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- src/kernel/cache.rs
+++ src/kernel/cache.rs
@@ -1,9 +1,13 @@
 use std::cell::OnceCell;
 use std::fs::File;
 use std::path::Path;
+use std::path::PathBuf;
 use std::rc::Rc;
 
+use crate::elf::ElfResolver;
+use crate::elf::ElfResolverData;
 use crate::file_cache::FileCache;
+use crate::pathlike::PathLike;
 use crate::util::OnceCellExt as _;
 use crate::ErrorExt as _;
 use crate::Result;
@@ -13,15 +17,55 @@ use super::KsymResolver;
 
 
 /// A cache for kernel related data.
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub(crate) struct KernelCache {
+    /// Cache of ELF files.
+    elf_cache: FileCache<ElfResolverData>,
     /// `/proc/kallsyms` cache.
     ksym_cache: FileCache<Rc<KsymResolver>>,
     /// The system's KASLR offset.
     kaslr_offset: OnceCell<u64>,
+    #[cfg(feature = "dwarf")]
+    debug_dirs: Rc<[PathBuf]>,
 }
 
 impl KernelCache {
+    #[cfg(feature = "dwarf")]
+    pub fn new(debug_dirs: Rc<[PathBuf]>) -> Self {
+        Self {
+            elf_cache: FileCache::default(),
+            ksym_cache: FileCache::default(),
+            kaslr_offset: OnceCell::default(),
+            debug_dirs,
+        }
+    }
+
+    #[cfg(not(feature = "dwarf"))]
+    pub fn new() -> Self {
+        Self {
+            elf_cache: FileCache::default(),
+            ksym_cache: FileCache::default(),
+            kaslr_offset: OnceCell::default(),
+        }
+    }
+
+    fn maybe_debug_dirs(&self, debug_syms: bool) -> Option<&[PathBuf]> {
+        #[cfg(feature = "dwarf")]
+        let debug_dirs = &self.debug_dirs;
+        #[cfg(not(feature = "dwarf"))]
+        let debug_dirs = &[];
+        debug_syms.then_some(debug_dirs)
+    }
+
+    pub fn elf_resolver<'slf>(
+        &'slf self,
+        path: &dyn PathLike,
+        debug_syms: bool,
+    ) -> Result<&'slf Rc<ElfResolver>> {
+        self.elf_cache
+            .elf_resolver(path, self.maybe_debug_dirs(debug_syms))
+    }
+
     fn create_ksym_resolver(&self, path: &Path, file: &File) -> Result<Rc<KsymResolver>> {
         let resolver = KsymResolver::load_from_reader(file, path)?;
         let resolver = Rc::new(resolver);
