502e06601ea7b2b8d7ea9fcadcadcf3603cf6c1d
Daniel Müller <deso@posteo.net>
2026-08-31 09:32:41 -0700

    build(deps): bump grev from 0.1.4 to 0.2.0
    Bumps [grev](https://github.com/d-e-s-o/grev) from 0.1.4 to 0.2.0.
    - [Release notes](https://github.com/d-e-s-o/grev/releases)
    - [Changelog](https://github.com/d-e-s-o/grev/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/d-e-s-o/grev/compare/v0.1.4...v0.2.0)
    
    ---
    updated-dependencies:
    - dependency-name: grev
      dependency-version: 0.2.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- cli/build.rs
+++ cli/build.rs
@@ -2,21 +2,18 @@
 
 use std::env;
 
-use anyhow::Result;
-
-use grev::git_revision_auto;
+use grev::git_revision;
 
 
-fn main() -> Result<()> {
-    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    if let Some(git_rev) = git_revision_auto(dir)? {
-        println!(
-            "cargo:rustc-env=VERSION={} ({})",
-            env!("CARGO_PKG_VERSION"),
-            git_rev
-        );
+fn main() {
+    let manifest_dir =
+        env::var_os("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR variable not set");
+    let pkg_version = env::var("CARGO_PKG_VERSION").expect("CARGO_PKG_VERSION variable not set");
+
+    let git_rev = git_revision(manifest_dir).expect("failed to query Git revision");
+    if let Some(git_rev) = git_rev {
+        println!("cargo:rustc-env=VERSION={pkg_version} ({git_rev})");
     } else {
-        println!("cargo:rustc-env=VERSION={}", env!("CARGO_PKG_VERSION"));
+        println!("cargo:rustc-env=VERSION={pkg_version}");
     }
-    Ok(())
 }
